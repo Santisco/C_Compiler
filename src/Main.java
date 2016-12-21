7cf2033e955d6eccb4914372d8406da939c62dee
skylover888 <154579704@qq.com>
2016-12-22 00:07:01 +0800

    数组使用

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,36 @@ import Token.Token;
 
 public class Main {
 
-	public static void main(String[] args){
 	
 		
-		Parser parser=new Parser();
-		System.out.println(
-			parser.Parser("void main(){"
-					+ "char a='12s3  fd456';"
-					+ ""
-					+ "}"		
-					+ "}   ")
-		);
-		
-		for(int i=0;i<Quadruples.count;i++)
-			System.out.println("("+Quadruples.quadruples[i].ope
-					+","+Quadruples.quadruples[i].arg1
-					+","+Quadruples.quadruples[i].arg2
-					+","+Quadruples.quadruples[i].res+")");
-		
-	}
+		public static void main(String[] args){
+			
+			
+			Parser parser=new Parser();
+			System.out.println(
+				parser.Parser("void main(){"
+						+ " int i;"
+						+ "int b[3] = {\"21312\", \"waduiog\", \"awd\"};"
+						+ "int a = 3+4;"
+						+ "int c = 3;"
+						+ "c = a+b;"
+						+ "while(i<1){"
+						+ "char b[3][4][5][6] = {\"gaiskuyhdgva\"};"
+						+ "}"
+						+ "while(i<9){"
+						+ "printf(ddd);"
+						+ "}"	
+						+ "while(i<9)"
+						+ "}    ")
+			);
+			System.out.println(parser.tagX);
+			System.out.println(parser.tagB);
+			System.out.println(parser.tagR);
+			for(int i=0;i<Quadruples.count;i++)
+				System.out.println("("+Quadruples.quadruples[i].ope
+						+","+Quadruples.quadruples[i].arg1
+						+","+Quadruples.quadruples[i].arg2
+						+","+Quadruples.quadruples[i].res+")");
+			
+		}
 }
